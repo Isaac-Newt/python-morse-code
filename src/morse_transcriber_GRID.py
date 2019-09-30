0d508d5d81a9fe66214b70dd3f9e08c1323199e3
isaac-newt <isaac.newt.list@icloud.com>
2019-09-29 20:43:24 -0500

    Layout tweaks, trying out Gtk.Grid

--- src/morse_transcriber_GRID.py
+++ src/morse_transcriber_GRID.py
@@ -37,7 +37,7 @@ class MainWindow(Gtk.Window):
         # Window padding
         self.set_border_width(15)
         self.set_default_size(325, 150)
-        self.main_area = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        self.main_area = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
         self.add(self.main_area)
 
         # Build window contents
@@ -45,27 +45,25 @@ class MainWindow(Gtk.Window):
 
     def build_main_view(self):
         """Build the Page"""
-        self.letters_to_morse = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
+        self.letters_to_morse = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
 
         # Input form
-        # self.text_input = Gtk.TextView()
-        # self.text_input_buffer = self.text_input.get_buffer()
         self.text_input = Gtk.Entry()
         input_box = self.build_text_box("Enter Text:", self.text_input)
-        self.letters_to_morse.pack_start(input_box, True, True, 0)
+        self.letters_to_morse.add(input_box)
 
         # Submit button
         self.submit_button = Gtk.Button(label="Convert")
         self.submit_button.connect("clicked", self.display_morse)
-        self.letters_to_morse.pack_start(self.submit_button, True, True, 0)
+        self.letters_to_morse.add(self.submit_button)
 
         # Output area
         self.morse_output = Gtk.Entry()
         self.morse_output.set_editable(False)
         output_box = self.build_text_box("Morse Transcription:", self.morse_output)
-        self.letters_to_morse.pack_start(output_box, True, True, 0)
+        self.letters_to_morse.add(output_box)
 
-        self.main_area.pack_start(self.letters_to_morse, True, True, 0)
+        self.main_area.add(self.letters_to_morse)
 
     def build_text_box(self, label_text: str, box_function):
         """Create a Gtk.Entry() element (i.e. text box)"""
@@ -83,9 +81,7 @@ class MainWindow(Gtk.Window):
     def display_morse(self, input_text):
         """Display converted text in the output box"""
         input_text = str(self.text_input.get_text())
-        # input_text = str(self.text_input_buffer.get_text(self.text_input_buffer.get_start_iter(), self.text_input_buffer.get_end_iter(), True))
         input_text = input_text.lower()
-        # print(input_text)
         morse_string = self.to_morse(input_text)
         self.morse_output.set_text(morse_string)
 
